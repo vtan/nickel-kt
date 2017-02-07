afc3e9a69d93e81f36b7ddd511b91fd28d26707a
Viktor Tanyi <tanyi.viktor@gmail.com>
2017-02-07 19:24:37 +0100

    Remove Jersey package scanning so the app can be run via `java -jar...`

--- src/main/kotlin/vtan/nickel/Application.kt
+++ src/main/kotlin/vtan/nickel/Application.kt
@@ -1,10 +1,10 @@
 package vtan.nickel
 
-import org.glassfish.jersey.logging.LoggingFeature
 import org.glassfish.jersey.server.ResourceConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.stereotype.Component
+import vtan.nickel.application.ExpenseRestResource
 import javax.ws.rs.ApplicationPath
 
 @SpringBootApplication
@@ -17,8 +17,7 @@ fun main(args: Array<String>) = SpringApplication.run(Application::class.java, *
 class JerseyConfig : ResourceConfig() {
 
     init {
-        packages("vtan.nickel")
-        register(LoggingFeature::class.java)
+        registerClasses(ExpenseRestResource::class.java)
     }
 
 }
